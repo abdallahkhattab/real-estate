b6ab95736afb87bb891a784e162dbde0c0c49957
abdallah <boodykhattab97@gmail.com>
2024-12-06 23:13:38 +0200

    33

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Site\SiteController;
 
@@ -21,6 +22,11 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Artisan::call('cache:clear');
+Artisan::call('config:cache');
+Artisan::call('view:clear');
+return "Cleared!";
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
